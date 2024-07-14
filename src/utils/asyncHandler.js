85d51eb4152f1f7a27a035fa2e3809fd04e71dfa
ChiragKaushik21 <chiragka2002@gmail.com>
2024-07-14 22:22:28 +0530

    u

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,13 +1,13 @@
 // promises
 const asyncHandler = (requestHandler) => {
-    (req, res, next) => {
+    return (req, res, next) => {
         Promise.resolve(requestHandler(req, res, next))
         .catch((err) => next(err))
     }
 }
 
 
-export { asyncHandler }
+export default asyncHandler 
 
 // try and catch
 // const asyncHandler = (fn) = async (req, res, next) => {
